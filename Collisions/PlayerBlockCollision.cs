a6f422d59b6f05c8b0eae592d9789dafa7c9faa4
Amarth Gul <amarthgul@gmail.com>
2021-05-09 00:47:32 -0400

    Add files via upload

---
+++ Collisions/PlayerBlockCollision.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace HappyDungeon
+{
+    class PlayerBlockCollision
+    {
+        private Game1 game;
+        List<IBlock> blockList; 
+
+        public PlayerBlockCollision(Game1 G)
+        {
+            game = G;
+            blockList = G.staticBlockList; 
+        }
+
+        public bool ValidMove(Rectangle PlayerCollisionRect)
+        {
+            bool result = true; 
+
+            foreach(IBlock block in blockList)
+            {
+                Rectangle Intersection = Rectangle.Intersect(PlayerCollisionRect, block.GetRectangle());
+
+                if (Intersection.Width > 0) { 
+
+                    result = false;
+
+                }
+            }
+
+            return result;
+        }
+    }
+}
